795410377076dab313e5c65724e214457e701abe
Jonathan Richmond <richmo11@purdue.edu>
2025-01-27 20:26:41 -0500

    Created new PhD script repository

---
+++ CR3BPFamilies/EML1Lyapunovs.jl
@@ -0,0 +1,15 @@
+"""
+Script for Earth-Moon CR3BP L1 Lyapunov orbit family
+
+Author: Jonathan Richmond
+C: 1/27/25
+"""
+module EML1Lyap
+println()
+
+using MBD
+
+systemData = MBD.CR3BPSystemData("Earth", "Moon")
+
+println()
+end
