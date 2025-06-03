846e1412e77709164f79b5e1c11f77892020be5a
Jonathan Richmond <richmo11@purdue.edu>
2025-06-03 17:03:33 -0400

    Added exporting and plotting capabilities

--- CR3BPFamilies/EML1Lyapunovs.jl
+++ CR3BPFamilies/EML1Lyapunovs.jl
@@ -3,15 +3,16 @@ Script for Earth-Moon CR3BP L1 Lyapunov orbit family
 
 Author: Jonathan Richmond
 C: 2/4/25
-U: 5/29/25
+U: 6/3/25
 """
 module EML1Lyap
 println()
 
-using MBD, MATLAB
+using MBD, GLMakie
 
 include("../CR3BPTargeters/PlanarPerpJC.jl")
 include("../Utilities/Export.jl")
+include("../Utilities/Plot.jl")
 
 systemData = MBD.CR3BPSystemData("Earth", "Moon")
 dynamicsModel = MBD.CR3BPDynamicsModel(systemData)
@@ -19,6 +20,7 @@ Earth = systemData.primaryData[1]
 Moon = systemData.primaryData[2]
 LagrangePoints::Vector{Vector{Float64}} = [getEquilibriumPoint(dynamicsModel, l) for l = 1:5]
 
+propagator = MBD.Propagator()
 targeter = PlanarPerpJCTargeter(dynamicsModel)
 
 (initialStateGuess::Vector{Float64}, tSpanGuess::Vector{Float64}) = getLinearVariation(dynamicsModel, 1, LagrangePoints[1], [0.005, 0, 0])
@@ -32,7 +34,7 @@ println("\nConverged Orbit 2:\n\tState:$(solution2.nodes[1].state.data[1:6])\n\t
 engine = MBD.JacobiConstantContinuationEngine(solution1, solution2, -1E-4, -1E-2)
 ydot0JumpCheck = MBD.BoundingBoxJumpCheck("Initial State", [NaN NaN; -2.5 0])
 addJumpCheck!(engine, ydot0JumpCheck)
-MoonEndCheck = MBD.BoundingBoxContinuationEndCheck("Initial State", [L1[1] getPrimaryPosition(dynamicsModel, 2)[1]-Moon.bodyRadius/getCharLength(systemData); NaN NaN])
+MoonEndCheck = MBD.BoundingBoxContinuationEndCheck("Initial State", [LagrangePoints[1][1] getPrimaryState(dynamicsModel, 2)[1]-Moon.bodyRadius/getCharLength(systemData); NaN NaN])
 addEndCheck!(engine, MoonEndCheck)
 
 println()
@@ -50,18 +52,32 @@ for s::Int64 in 1:getNumMembers(solutions)
 end
 eigenSort!(family)
 
-# exportData(family, "FamilyData/EML1Lyapunovs.csv")
-mf = MATLAB.MatFile("FamilyData/CR3BPEML1Lyapunovs.mat", "w")
-exportCR3BPFamily()
-MATLAB.close(mf)
+println("\nExporting family data...")
+fullExportCR3BPFamily(family, "FamilyData/CR3BPEML1Lyapunovs.mat", "FamilyData/CR3BPEML1Lyapunovs.csv")
 
+
+# TO DO
 # println("\nTesting interpolation...")
 # testOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(targeter, "FamilyData/EML1Lyapunovs.csv", "JC", 3.0)
 # println("\nTest Orbit:\n\tState:$(testOrbit.initialCondition)\n\tPeriod: $(testOrbit.period)\n\tJC: $(getJacobiConstant(testOrbit))\n\tStability Index: $(getStabilityIndex(testOrbit))")
 
-# mf = MATLAB.MatFile("Output/CR3BPTraj.mat", "w")
-# exportCR3BPOrbit(testOrbit, dynamicsModel, mf, :trajCR3BP)
-# MATLAB.close(mf)
+
+println("\nPlotting orbit...")
+plotOrbit::Int64 = getNumMembers(family)
+orbitArc::MBD.CR3BPArc = propagate(propagator, family.initialConditions[plotOrbit], [0, family.periods[plotOrbit]], dynamicsModel)
+xData::Vector{Float64} = Vector{Float64}(undef, getStateCount(orbitArc))
+yData::Vector{Float64} = Vector{Float64}(undef, getStateCount(orbitArc))
+for s::Int64 in 1:getStateCount(orbitArc)
+    xData[s] = orbitArc.states[s][1]
+    yData[s] = orbitArc.states[s][2]
+end
+(figure, axis) = set2DPlotParameters(L"Earth-Moon $L_{1}$ Lyapunov ($JC=%$(round(getJacobiConstant(dynamicsModel, family.initialConditions[plotOrbit]); digits = 4))$)", L"$x$ [ndim]", L"$y$ [ndim]")
+GLMakie.lines!(axis, xData, yData, color = :white, label = L"$L_{1}$ Lyapunov Orbit")
+GLMakie.scatter!(axis, LagrangePoints[1][1], LagrangePoints[1][2], color = :red, markersize = 5, label = L"$L_{1}$" => (; markersize = 20))
+GLMakie.scatter!(axis, LagrangePoints[2][1], LagrangePoints[2][2], color = :orange, markersize = 5, label = L"$L_{2}$" => (; markersize = 20))
+GLMakie.scatter!(axis, getPrimaryState(dynamicsModel, 1)[1], getPrimaryState(dynamicsModel, 1)[2], color = :blue, markerspace = :data, markersize = Earth.bodyRadius/getCharLength(systemData), label = L"\mathrm{Earth}" => (; markersize = 20))
+GLMakie.scatter!(axis, getPrimaryState(dynamicsModel, 2)[1], getPrimaryState(dynamicsModel, 2)[2], color = :gray, markerspace = :data, markersize = Moon.bodyRadius/getCharLength(systemData), label = L"\mathrm{Moon}" => (; markersize = 20))
+GLMakie.Legend(figure[1,2], axis)
 
 println()
 end
--- Utilities/Export.jl
+++ Utilities/Export.jl
@@ -3,13 +3,13 @@ Export utility functions
 
 Author: Jonathan Richmond
 C: 2/19/25
-U: 5/29/25
+U: 6/3/25
 """
 
-using MBD, MATLAB
+using MBD, CSV, DataFrames, LinearAlgebra, MATLAB
 
-export exportBCR4BP12Trajectory, exportBCR4BP12Trajectory, exportCR3BPFamily, exportCR3BPOrbit
-export exportCR3BPTrajectory
+export CSVExportCR3BPFamily, exportBCR4BP12Trajectory, exportBCR4BP12Trajectory, exportCR3BPOrbit
+export exportCR3BPTrajectory, fullExportCR3BPFamily, MATExportCR3BPFamily
 
 """
     BCR4BP12Traj(x, y, z, xdot, ydot, zdot, theta4, t)
@@ -24,7 +24,7 @@ BCR4BP P1-P2 trajectory export object
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta4::Vector{Float64}`: theta4 data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
 """
 struct BCR4BP12Traj
     x::Vector{Float64}                                                  # x data [ndim]
@@ -56,7 +56,7 @@ BCR4BP P4-B1 trajectory export object
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta2::Vector{Float64}`: theta2 data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
 """
 struct BCR4BP41Traj
     x::Vector{Float64}                                                  # x data [ndim]
@@ -76,7 +76,43 @@ struct BCR4BP41Traj
 end
 
 """
-    CR3BPTraj(x, y, z, xdot, ydot, zdot, t)
+    CR3BPOrb(x, y, z, xdot, ydot, zdot, t, P, JC, varsig)
+
+CR3BP orbit export object
+
+# Arguments
+- `x::Vector{Float64}`: x data [ndim]
+- `y::Vector{Float64}`: y data [ndim]
+- `z::Vector{Float64}`: z data [ndim]
+- `xdot::Vector{Float64}`: xdot data [ndim]
+- `ydot::Vector{Float64}`: ydot data [ndim]
+- `zdot::Vector{Float64}`: zdot data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
+- `P::Float64`: Period [ndim]
+- `JC::Float64`: Jacobi constant
+- `varsig::Float64`: Stability index
+"""
+struct CR3BPOrb
+    JC::Float64                                                         # Jacobi constant
+    P::Float64                                                          # Period [ndim]
+    t::Vector{Float64}                                                  # time data [ndim]
+    varsig::Float64                                                     # Stability index
+    x::Vector{Float64}                                                  # x data [ndim]
+    xdot::Vector{Float64}                                               # xdot data [ndim]
+    y::Vector{Float64}                                                  # y data [ndim]
+    ydot::Vector{Float64}                                               # ydot data [ndim]
+    z::Vector{Float64}                                                  # z data [ndim]
+    zdot::Vector{Float64}                                               # zdot data [ndim]
+
+    function CR3BPOrb(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64}, P::Float64, JC::Float64, varsig::Float64)
+        this = new(JC, P, t, varsig, x, xdot, y, ydot, z, zdot)
+
+        return this
+    end
+end
+
+"""
+    CR3BPTraj(x, y, z, xdot, ydot, zdot, t, JC)
 
 CR3BP trajectory export object
 
@@ -87,22 +123,60 @@ CR3BP trajectory export object
 - `xdot::Vector{Float64}`: xdot data [ndim]
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
+- `JC::Float64`: Jacobi constant
 """
 struct CR3BPTraj
-    x::Vector{Float64}                                                  # x data [ndim]
-    y::Vector{Float64}                                                  # y data [ndim]
-    z::Vector{Float64}                                                  # z data [ndim]
-    xdot::Vector{Float64}                                               # xdot data [ndim]
-    ydot::Vector{Float64}                                               # ydot data [ndim]
-    zdot::Vector{Float64}                                               # zdot data [ndim]
+    JC::Float64                                                         # Jacobi constant
     t::Vector{Float64}                                                  # time data [ndim]
+    x::Vector{Float64}                                                  # x data [ndim]
+    xdot::Vector{Float64}                                               # xdot data [ndim]
+    y::Vector{Float64}                                                  # y data [ndim]
+    ydot::Vector{Float64}                                               # ydot data [ndim]
+    z::Vector{Float64}                                                  # z data [ndim]
+    zdot::Vector{Float64}                                               # zdot data [ndim]
 
-    function CR3BPTraj(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64})
-        this = new(x, y, z, xdot, ydot, zdot, t)
+    function CR3BPTraj(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64}, JC::Float64)
+        this = new(JC, t, x, xdot, y, ydot, z, zdot)
 
         return this
     end
+end
+
+"""
+    CSVExportCR3BPFamily(family, file)
+
+Export CR3BP family data to CSV file
+
+# Arguments
+- `family::CR3BPOrbitFamily`: CR3BP periodic orbit family object
+- `file::String`: CSV file name
+"""
+function CSVExportCR3BPFamily(family::MBD.CR3BPOrbitFamily, file::String)
+    nMem::Int64 = getNumMembers(family)
+    x::Vector{Float64} = Vector{Float64}(undef, nMem)
+    y::Vector{Float64} = Vector{Float64}(undef, nMem)
+    z::Vector{Float64} = Vector{Float64}(undef, nMem)
+    xdot::Vector{Float64} = Vector{Float64}(undef, nMem)
+    ydot::Vector{Float64} = Vector{Float64}(undef, nMem)
+    zdot::Vector{Float64} = Vector{Float64}(undef, nMem)
+    P::Vector{Float64} = Vector{Float64}(undef, nMem)
+    JC::Vector{Float64} = Vector{Float64}(undef, nMem)
+    varsig::Vector{Float64} = Vector{Float64}(undef, nMem)
+    for o::Int64 = 1:nMem
+        orbit::MBD.CR3BPPeriodicOrbit = getMember(family, o)
+        x[o] = orbit.initialCondition[1]
+        y[o] = orbit.initialCondition[2]
+        z[o] = orbit.initialCondition[3]
+        xdot[o] = orbit.initialCondition[4]
+        ydot[o] = orbit.initialCondition[5]
+        zdot[o] = orbit.initialCondition[6]
+        P[o] = orbit.period
+        JC[o] = getJacobiConstant(orbit)
+        varsig[o] = getStabilityIndex(orbit)
+    end
+    familyData::DataFrames.DataFrame = DataFrames.DataFrame("x" => x, "y" => y, "z" => z, "xdot" => xdot, "ydot" => ydot, "zdot" => zdot, "Period" => P, "JC" => JC, "Stability" => varsig)
+    CSV.write(file, familyData)
 end
 
 """
@@ -118,7 +192,7 @@ Export BCR4BP P1-P2 trajectory data to MAT file
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta4::Vector{Float64}`: theta4 data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
 """
@@ -140,7 +214,7 @@ Export BCR4BP P4-B1 trajectory data to MAT file
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta2::Vector{Float64}`: theta2 data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
 """
@@ -150,31 +224,18 @@ function exportBCR4BP41Trajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vec
 end
 
 """
-    exportCR3BPFamily(family)
-
-Export CR3BP family data to MAT file
-
-# Arguments
-- ``
-"""
-function exportCR3BPFamily(family::MBD.CR3BPOrbitFamily)
-
-end
-
-"""
-    exportCR3BPOrbit(orbit, dynamicsModel, file, name)
+    exportCR3BPOrbit(orbit, file, name)
 
 Export CR3BP orbit data to MAT file
 
 # Arguments
 - `orbit::CR3BPPeriodicOrbit`: CR3BP periodic orbit object
-- `dynamicsModel::CR3BPDynamicsModel`: CR3BP dynamics model object
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
 """
-function exportCR3BPOrbit(orbit::MBD.CR3BPPeriodicOrbit, dynamicsModel::MBD.CR3BPDynamicsModel, file::MATLAB.MatFile, name::Symbol)
+function exportCR3BPOrbit(orbit::MBD.CR3BPPeriodicOrbit, file::MATLAB.MatFile, name::Symbol)
     propagator = MBD.Propagator()
-    orbitArc::MBD.CR3BPArc = propagate(propagator, orbit.initialCondition, [0, orbit.period], dynamicsModel)
+    orbitArc::MBD.CR3BPArc = propagate(propagator, orbit.initialCondition, [0, orbit.period], orbit.dynamicsModel)
     nStates::Int64 = getStateCount(orbitArc)
     x::Vector{Float64} = zeros(Float64, nStates)
     y::Vector{Float64} = zeros(Float64, nStates)
@@ -193,11 +254,37 @@ function exportCR3BPOrbit(orbit::MBD.CR3BPPeriodicOrbit, dynamicsModel::MBD.CR3B
         zdot[s] = state[6]
         t[s] = getTimeByIndex(orbitArc, s)
     end
-    exportCR3BPTrajectory(x, y, z, xdot, ydot, zdot, t, file, name)
+    JC::Float64 = MBD.getJacobiConstant(orbit)
+    varsig::Float64 = getStabilityIndex(orbit)
+    exportCR3BPOrbit(x, y, z, xdot, ydot, zdot, t, orbit.period, JC, varsig, file, name)
 end
 
 """
-    exportCR3BPTrajectory(x, y, z, xdot, ydot, zdot, t, file, name)
+    exportCR3BPOrbit(x, y, z, xdot, ydot, zdot, t, P, JC, varsig, file, name)
+
+Export CR3BP orbit data to MAT file
+
+# Arguments
+- `x::Vector{Float64}`: x data [ndim]
+- `y::Vector{Float64}`: y data [ndim]
+- `z::Vector{Float64}`: z data [ndim]
+- `xdot::Vector{Float64}`: xdot data [ndim]
+- `ydot::Vector{Float64}`: ydot data [ndim]
+- `zdot::Vector{Float64}`: zdot data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
+- `P::Float64`: Period [ndim]
+- `JC::Float64`: Jacobi constant
+- `varsig::Float64`: Stability index
+- `file::MatFile`: MAT file
+- `name::Symbol`: Export object name
+"""
+function exportCR3BPOrbit(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64}, P::Float64, JC::Float64, varsig::Float64, file::MATLAB.MatFile, name::Symbol)
+    orbit = CR3BPOrb(x, y, z, xdot, ydot, zdot, t, P, JC, varsig)
+    MATLAB.put_variable(file, name, orbit)
+end
+
+"""
+    exportCR3BPTrajectory(x, y, z, xdot, ydot, zdot, t, JC, file, name)
 
 Export CR3BP trajectory data to MAT file
 
@@ -209,10 +296,44 @@ Export CR3BP trajectory data to MAT file
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `t::Vector{Float64}`: time data [ndim]
+- `JC::Float64`: Jacobi constant
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
 """
-function exportCR3BPTrajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64}, file::MATLAB.MatFile, name::Symbol)
-    traj = CR3BPTraj(x, y, z, xdot, ydot, zdot, t)
+function exportCR3BPTrajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, t::Vector{Float64}, JC::Float64, file::MATLAB.MatFile, name::Symbol)
+    traj = CR3BPTraj(x, y, z, xdot, ydot, zdot, t, JC)
     MATLAB.put_variable(file, name, traj)
+end
+
+"""
+    fullExportCR3BPFamily(family, MATFile, CSVFile)
+
+Export CR3BP family data to MAT and CSV files
+
+# Arguments
+- `family::CR3BPOrbitFamily`: CR3BP periodic orbit family object
+- `MATFile::String`: MAT file name
+- `CSVFile::String`: CSV file name
+"""
+function fullExportCR3BPFamily(family::MBD.CR3BPOrbitFamily, MATFile::String, CSVFile::String)
+    mf = MATLAB.MatFile(MATFile, "w")
+    MATExportCR3BPFamily(family, mf)
+    MATLAB.close(mf)
+    CSVExportCR3BPFamily(family, CSVFile)
+end
+
+"""
+    MATExportCR3BPFamily(family, file)
+
+Export CR3BP family data to MAT file
+
+# Arguments
+- `family::CR3BPOrbitFamily`: CR3BP periodic orbit family object
+- `file::MatFile`: MAT file
+"""
+function MATExportCR3BPFamily(family::MBD.CR3BPOrbitFamily, file::MATLAB.MatFile)
+    for o::Int64 = 1:getNumMembers(family)
+        orbit::MBD.CR3BPPeriodicOrbit = getMember(family, o)
+        exportCR3BPOrbit(orbit, file, Symbol("orbit"*string(o)))
+    end
 end
