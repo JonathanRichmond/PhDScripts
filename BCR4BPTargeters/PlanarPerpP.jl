6e7c0b9ee70098ab0b620fc1de11cec38934db2d
Jonathan Richmond <richmo11@purdue.edu>
2025-06-09 16:45:25 -0400

    Added BCR4BP12 periodic orbit capabilities

--- BCR4BPTargeters/PlanarPerpP.jl
+++ BCR4BPTargeters/PlanarPerpP.jl
@@ -3,13 +3,13 @@ Period perpendicular crossing targeter for BCR4BP planar orbits
 
 Author: Jonathan Richmond
 C: 4/9/25
-U: 6/5/25
+U: 6/9/25
 """
 
-using MBD, DifferentialEquations, StaticArrays
+using MBD, DifferentialEquations, Logging, StaticArrays
 
 export PlanarPerpP12Targeter
-export correct, getMonodromy, getPeriod, propagateState#, getIndividualPeriodicOrbit, interpOrbit
+export correct, getMonodromy, getPeriod, getResonantOrbit, homotopy, propagateState
 
 """
     PlanarPerpP12Targeter(dynamicsModel)
@@ -19,7 +19,7 @@ BCR4BP P1-P2 planar perpendicular crossing period targeter object
 # Arguments
 - `dynamicsModel::BCR4BP12DynamicsModel`: BCR4BP P1-P2 dynamics model object
 """
-struct PlanarPerpP12Targeter <: MBD.AbstractTargeter
+struct PlanarPerpP12Targeter
     dynamicsModel::MBD.BCR4BP12DynamicsModel                            # BCR4BP P1-P2 dynamics model object
 
     function PlanarPerpP12Targeter(dynamicsModel::MBD.BCR4BP12DynamicsModel)
@@ -99,6 +99,69 @@ Return orbit period
 """
 function getPeriod(targeter::PlanarPerpP12Targeter, solution::MBD.BCR4BP12MultipleShooterProblem)
     return 2*solution.segments[1].TOF.data[1]
+end
+
+"""
+    getResonantOrbit(targeter, initialOrbit, p, q; Deltaeps, tol, refTol, JTol)
+
+Return synodic-resonant periodic orbit
+
+# Arguments
+- `targeter::PlanarPerpP12Targeter`: BCR4BP P1-P2 planar perpendicular crossing period targeter object
+- `initialOrbit::CR3BPPeriodicOrbit`: CR3BP periodic orbit initial guess
+- `p::Int64`: Orbit revolutions
+- `q::Int64`: Synodic revolutions
+- `Deltaeps::Float64`: Homotopy parameter step (Sun mass)
+- `tol::Float64`: Convergence tolerance (default = 1E-10)
+- `refTol::Float64`: Refined solution convergence tolerance (default = 1E-11)
+- `JTol::Float64`: Jacobian accuracy tolerance (default = 2E-3)
+"""
+function getResonantOrbit(targeter::PlanarPerpP12Targeter, initialOrbit::MBD.CR3BPPeriodicOrbit, p::Int64, q::Int64; Deltaeps = 0.001, tol = 1E-11, refTol = 1E-11, JTol = 2E-3)
+    initialStateGuess::Vector{Float64} = push!(copy(initialOrbit.initialCondition), 0.0)
+    solution::MBD.BCR4BP12MultipleShooterProblem = homotopy(targeter, initialStateGuess, p, q, Deltaeps = Deltaeps, tol = tol, JTol = JTol)
+    refinedSolution::MBD.BCR4BP12MultipleShooterProblem = correct(targeter, solution.nodes[1].state.data[1:7], q*getSynodicPeriod(targeter.dynamicsModel), q, tol = refTol, JTol = JTol)
+    println("Converged $p:$q BCR4BP Orbit:\n\tIC:\t$(refinedSolution.nodes[1].state.data[1:7])\n\tP:\t$(getPeriod(targeter, refinedSolution))\n")
+    orbit = MBD.BCR4BP12PeriodicOrbit(dynamicsModel, refinedSolution.nodes[1].state.data[1:7], getPeriod(targeter, refinedSolution), getMonodromy(targeter, refinedSolution))
+
+    return orbit
+end
+
+"""
+    homotopy(targeter, CR3BPq0, p, q; Deltaeps, tol, JTol)
+
+Return continued BCR4BP P1-P2 solution
+
+# Arguments
+- `targeter::PlanarPerpP12Targeter`: BCR4BP P1-P2 planar perpendicular crossing period targeter object
+- `CR3BPq0::Vector{Float64}`: CR3BP initial condition [ndim]
+- `p::Int64`: Orbit revolutions
+- `q::Int64`: Synodic revolutions
+- `Deltaeps::Float64`: Homotopy parameter step (Sun mass)
+- `tol::Float64`: Convergence tolerance (default = 1E-11)
+- `JTol::Float64`: Jacobian accuracy tolerance (default = 2E-3)
+"""
+function homotopy(targeter::PlanarPerpP12Targeter, CR3BPq0::Vector{Float64}, p::Int64, q::Int64; Deltaeps::Float64 = 0.001, tol::Float64 = 1E-11, JTol::Float64 = 2E-3)
+    initialStateGuess::Vector{Float64} = copy(CR3BPq0)
+    homoSystemData::MBD.BCR4BPSystemData = MBD.shallowClone(targeter.dynamicsModel.systemData)
+    homoDynamicsModel = MBD.BCR4BP12DynamicsModel(homoSystemData)
+    homoTargeter = PlanarPerpP12Targeter(homoDynamicsModel)
+    SunGravParam::Float64 = copy(homoSystemData.primaryData[3].gravParam)
+    homoSystemData.primaryData[3].gravParam = 0.0
+    targetP::Float64 = q*getSynodicPeriod(homoDynamicsModel)
+    solution::MBD.BCR4BP12MultipleShooterProblem = correct(homoTargeter, CR3BPq0, targetP, q; tol = tol, JTol = JTol)
+    println("\nConverged $p:$q CR3BP Orbit:\n\tIC:\t$(solution.nodes[1].state.data[1:7])\n\tP:\t$(getPeriod(homoTargeter, solution))\n")
+    
+    epsilon = Deltaeps
+    while epsilon < 1.0
+        homoSystemData.primaryData[3].gravParam = epsilon*SunGravParam
+        targetP = q*getSynodicPeriod(homoDynamicsModel)
+        initialStateGuess = solution.nodes[1].state.data[1:7]
+        solution = correct(homoTargeter, initialStateGuess, targetP, q; tol = tol, JTol = JTol)
+        Logging.@info "Converged $p:$q Homotopy Orbit (epsilon = $epsilon):\n\tIC:\t$(solution.nodes[1].state.data[1:7])\n\tP:\t$(getPeriod(homoTargeter, solution))\n"
+        epsilon += Deltaeps
+    end
+
+    return solution
 end
 
 """
--- Utilities/Export.jl
+++ Utilities/Export.jl
@@ -3,16 +3,54 @@ Export utility functions
 
 Author: Jonathan Richmond
 C: 2/19/25
-U: 6/3/25
+U: 6/9/25
 """
 
 using MBD, CSV, DataFrames, LinearAlgebra, MATLAB
 
-export CSVExportCR3BPFamily, exportBCR4BP12Trajectory, exportBCR4BP12Trajectory, exportCR3BPOrbit
+export CSVExportCR3BPFamily, exportBCR4BP12Orbit, exportBCR4BP12Trajectory, exportCR3BPOrbit
 export exportCR3BPTrajectory, fullExportCR3BPFamily, MATExportCR3BPFamily
 
 """
-    BCR4BP12Traj(x, y, z, xdot, ydot, zdot, theta4, t)
+    BCR4BP12Orb(x, y, z, xdot, ydot, zdot, theta4, t, H, P, varsig)
+
+BCR4BP P1-P2 orbit export object
+
+# Arguments
+- `x::Vector{Float64}`: x data [ndim]
+- `y::Vector{Float64}`: y data [ndim]
+- `z::Vector{Float64}`: z data [ndim]
+- `xdot::Vector{Float64}`: xdot data [ndim]
+- `ydot::Vector{Float64}`: ydot data [ndim]
+- `zdot::Vector{Float64}`: zdot data [ndim]
+- `theta4::Vector{Float64}`: theta4 data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
+- `H::Vector{Float64}`: Hamiltonian data [ndim]
+- `P::Float64`: Period [ndim]
+- `varsig::Float64`: Stability index
+"""
+struct BCR4BP12Orb
+    H::Vector{Float64}                                                  # Hamiltonian data [ndim]
+    P::Float64                                                          # Period [ndim]
+    t::Vector{Float64}                                                  # Time data [ndim]
+    theta4::Vector{Float64}                                             # theta4 data [ndim]
+    varsig::Float64                                                     # Stability index
+    x::Vector{Float64}                                                  # x data [ndim]
+    xdot::Vector{Float64}                                               # xdot data [ndim]
+    y::Vector{Float64}                                                  # y data [ndim]
+    ydot::Vector{Float64}                                               # ydot data [ndim]
+    z::Vector{Float64}                                                  # z data [ndim]
+    zdot::Vector{Float64}                                               # zdot data [ndim]
+
+    function BCR4BP12Orb(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta4::Vector{Float64}, t::Vector{Float64}, H::Vector{Float64}, P::Float64, varsig::Float64)
+        this = new(H, P, t, theta4, varsig, x, xdot, y, ydot, z, zdot)
+
+        return this
+    end
+end
+
+"""
+    BCR4BP12Traj(x, y, z, xdot, ydot, zdot, theta4, t, H)
 
 BCR4BP P1-P2 trajectory export object
 
@@ -25,51 +63,21 @@ BCR4BP P1-P2 trajectory export object
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta4::Vector{Float64}`: theta4 data [ndim]
 - `t::Vector{Float64}`: Time data [ndim]
+- `H::Vector{Float64}`: Hamiltonian data [ndim]
 """
 struct BCR4BP12Traj
-    x::Vector{Float64}                                                  # x data [ndim]
-    y::Vector{Float64}                                                  # y data [ndim]
-    z::Vector{Float64}                                                  # z data [ndim]
-    xdot::Vector{Float64}                                               # xdot data [ndim]
-    ydot::Vector{Float64}                                               # ydot data [ndim]
-    zdot::Vector{Float64}                                               # zdot data [ndim]
+    H::Vector{Float64}                                                  # Hamiltonian data [ndim]
+    t::Vector{Float64}                                                  # Time data [ndim]
     theta4::Vector{Float64}                                             # theta4 data [ndim]
-    t::Vector{Float64}                                                  # time data [ndim]
+    x::Vector{Float64}                                                  # x data [ndim]
+    xdot::Vector{Float64}                                               # xdot data [ndim]
+    y::Vector{Float64}                                                  # y data [ndim]
+    ydot::Vector{Float64}                                               # ydot data [ndim]
+    z::Vector{Float64}                                                  # z data [ndim]
+    zdot::Vector{Float64}                                               # zdot data [ndim]
 
     function BCR4BP12Traj(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta4::Vector{Float64}, t::Vector{Float64})
-        this = new(x, y, z, xdot, ydot, zdot, theta4, t)
-
-        return this
-    end
-end
-
-"""
-    BCR4BP41Traj(x, y, z, xdot, ydot, zdot, theta2, t)
-
-BCR4BP P4-B1 trajectory export object
-
-# Arguments
-- `x::Vector{Float64}`: x data [ndim]
-- `y::Vector{Float64}`: y data [ndim]
-- `z::Vector{Float64}`: z data [ndim]
-- `xdot::Vector{Float64}`: xdot data [ndim]
-- `ydot::Vector{Float64}`: ydot data [ndim]
-- `zdot::Vector{Float64}`: zdot data [ndim]
-- `theta2::Vector{Float64}`: theta2 data [ndim]
-- `t::Vector{Float64}`: Time data [ndim]
-"""
-struct BCR4BP41Traj
-    x::Vector{Float64}                                                  # x data [ndim]
-    y::Vector{Float64}                                                  # y data [ndim]
-    z::Vector{Float64}                                                  # z data [ndim]
-    xdot::Vector{Float64}                                               # xdot data [ndim]
-    ydot::Vector{Float64}                                               # ydot data [ndim]
-    zdot::Vector{Float64}                                               # zdot data [ndim]
-    theta2::Vector{Float64}                                             # theta2 data [ndim]
-    t::Vector{Float64}                                                  # time data [ndim]
-
-    function BCR4BP41Traj(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta2::Vector{Float64}, t::Vector{Float64})
-        this = new(x, y, z, xdot, ydot, zdot, theta2, t)
+        this = new(H, t, theta4, x, xdot, y, ydot, z, zdot)
 
         return this
     end
@@ -95,7 +103,7 @@ CR3BP orbit export object
 struct CR3BPOrb
     JC::Float64                                                         # Jacobi constant
     P::Float64                                                          # Period [ndim]
-    t::Vector{Float64}                                                  # time data [ndim]
+    t::Vector{Float64}                                                  # Time data [ndim]
     varsig::Float64                                                     # Stability index
     x::Vector{Float64}                                                  # x data [ndim]
     xdot::Vector{Float64}                                               # xdot data [ndim]
@@ -128,7 +136,7 @@ CR3BP trajectory export object
 """
 struct CR3BPTraj
     JC::Float64                                                         # Jacobi constant
-    t::Vector{Float64}                                                  # time data [ndim]
+    t::Vector{Float64}                                                  # Time data [ndim]
     x::Vector{Float64}                                                  # x data [ndim]
     xdot::Vector{Float64}                                               # xdot data [ndim]
     y::Vector{Float64}                                                  # y data [ndim]
@@ -180,7 +188,71 @@ function CSVExportCR3BPFamily(family::MBD.CR3BPOrbitFamily, file::String)
 end
 
 """
-    exportBCR4BP12Trajectory(x, y, z, xdot, ydot, zdot, t, theta4, file, name)
+    exportBCR4BP12Orbit(orbit, file, name)
+
+Export BCR4BP P1-P2 orbit data to MAT file
+
+# Arguments
+- `orbit::BCR4BP12PeriodicOrbit`: BCR4BP P1-P2 periodic orbit object
+- `file::MatFile`: MAT file
+- `name::Symbol`: Export object name
+"""
+function exportBCR4BP12Orbit(orbit::MBD.BCR4BP12PeriodicOrbit, file::MATLAB.MatFile, name::Symbol)
+    propagator = MBD.Propagator()
+    orbitArc::MBD.BCR4BP12Arc = propagate(propagator, orbit.initialCondition, [0, orbit.period], orbit.dynamicsModel)
+    nStates::Int64 = getStateCount(orbitArc)
+    x::Vector{Float64} = zeros(Float64, nStates)
+    y::Vector{Float64} = zeros(Float64, nStates)
+    z::Vector{Float64} = zeros(Float64, nStates)
+    xdot::Vector{Float64} = zeros(Float64, nStates)
+    ydot::Vector{Float64} = zeros(Float64, nStates)
+    zdot::Vector{Float64} = zeros(Float64, nStates)
+    t::Vector{Float64} = zeros(Float64, nStates)
+    theta4::Vector{Float64} = zeros(Float64, nStates)
+    H::Vector{Float64} = zeros(Float64, nStates)
+    for s::Int64 in 1:nStates
+        state::Vector{Float64} = getStateByIndex(orbitArc, s)
+        x[s] = state[1]
+        y[s] = state[2]
+        z[s] = state[3]
+        xdot[s] = state[4]
+        ydot[s] = state[5]
+        zdot[s] = state[6]
+        theta4[s] = state[7]
+        t[s] = getTimeByIndex(orbitArc, s)
+        H[s] = getHamiltonian(orbit.dynamicsModel, state)
+    end
+    varsig::Float64 = getStabilityIndex(orbit)
+    exportBCR4BP12Orbit(x, y, z, xdot, ydot, zdot, theta4, t, H, orbit.period, varsig, file, name)
+end
+
+"""
+    exportBCR4BP12Orbit(x, y, z, xdot, ydot, zdot, theta4, t, H, P, varsig, file, name)
+
+Export BCR4BP P1-P2 orbit data to MAT file
+
+# Arguments
+- `x::Vector{Float64}`: x data [ndim]
+- `y::Vector{Float64}`: y data [ndim]
+- `z::Vector{Float64}`: z data [ndim]
+- `xdot::Vector{Float64}`: xdot data [ndim]
+- `ydot::Vector{Float64}`: ydot data [ndim]
+- `zdot::Vector{Float64}`: zdot data [ndim]
+- `theta4::Vector{Float64}`: theta4 data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
+- `H::Vector{Float64}`: Hamiltonian data [ndim]
+- `P::Float64`: Period [ndim]
+- `varsig::Float64`: Stability index
+- `file::MatFile`: MAT file
+- `name::Symbol`: Export object name
+"""
+function exportBCR4BP12Orbit(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta4::Vector{Float64}, t::Vector{Float64}, H::Vector{Float64}, P::Float64, varsig::Float64, file::MATLAB.MatFile, name::Symbol)
+    orbit = BCR4BP12Orb(x, y, z, xdot, ydot, zdot, theta4, t, H, P, varsig)
+    MATLAB.put_variable(file, name, orbit)
+end
+
+"""
+    exportBCR4BP12Trajectory(x, y, z, xdot, ydot, zdot, theta4, t, H, file, name)
 
 Export BCR4BP P1-P2 trajectory data to MAT file
 
@@ -193,33 +265,12 @@ Export BCR4BP P1-P2 trajectory data to MAT file
 - `zdot::Vector{Float64}`: zdot data [ndim]
 - `theta4::Vector{Float64}`: theta4 data [ndim]
 - `t::Vector{Float64}`: Time data [ndim]
+- `H::Vector{Float64}`: Hamiltonian data [ndim]
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
 """
-function exportBCR4BP12Trajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta4::Vector{Float64}, t::Vector{Float64}, file::MATLAB.MatFile, name::Symbol)
-    traj = BCR4BP12Traj(x, y, z, xdot, ydot, zdot, theta4, t)
-    MATLAB.put_variable(file, name, traj)
-end
-
-"""
-    exportBCR4BP41Trajectory(x, y, z, xdot, ydot, zdot, t, theta2, file, name)
-
-Export BCR4BP P4-B1 trajectory data to MAT file
-
-# Arguments
-- `x::Vector{Float64}`: x data [ndim]
-- `y::Vector{Float64}`: y data [ndim]
-- `z::Vector{Float64}`: z data [ndim]
-- `xdot::Vector{Float64}`: xdot data [ndim]
-- `ydot::Vector{Float64}`: ydot data [ndim]
-- `zdot::Vector{Float64}`: zdot data [ndim]
-- `theta2::Vector{Float64}`: theta2 data [ndim]
-- `t::Vector{Float64}`: Time data [ndim]
-- `file::MatFile`: MAT file
-- `name::Symbol`: Export object name
-"""
-function exportBCR4BP41Trajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta2::Vector{Float64}, t::Vector{Float64}, file::MATLAB.MatFile, name::Symbol)
-    traj = BCR4BP41Traj(x, y, z, xdot, ydot, zdot, theta2, t)
+function exportBCR4BP12Trajectory(x::Vector{Float64}, y::Vector{Float64}, z::Vector{Float64}, xdot::Vector{Float64}, ydot::Vector{Float64}, zdot::Vector{Float64}, theta4::Vector{Float64}, t::Vector{Float64}, H::Vector{Float64}, file::MATLAB.MatFile, name::Symbol)
+    traj = BCR4BP12Traj(x, y, z, xdot, ydot, zdot, theta4, t, H)
     MATLAB.put_variable(file, name, traj)
 end
 
@@ -295,7 +346,7 @@ Export CR3BP trajectory data to MAT file
 - `xdot::Vector{Float64}`: xdot data [ndim]
 - `ydot::Vector{Float64}`: ydot data [ndim]
 - `zdot::Vector{Float64}`: zdot data [ndim]
-- `t::Vector{Float64}`: time data [ndim]
+- `t::Vector{Float64}`: Time data [ndim]
 - `JC::Float64`: Jacobi constant
 - `file::MatFile`: MAT file
 - `name::Symbol`: Export object name
