162380e8d02c7fa6bb32200c7b5e45bb3831237d
Jonathan Richmond <richmo11@purdue.edu>
2025-06-11 13:10:43 -0400

    Making presentation

--- Scripts/ManifoldComparison.jl
+++ Scripts/ManifoldComparison.jl
@@ -28,16 +28,16 @@ CR3BPTargeter = PlanarPerpJCTargeter(CR3BPDynamicsModel)
 CR3BPP::Float64, targetP::Float64 = 6.2840049277323855, getSynodicPeriod(dynamicsModel)
 
 familyFile::String = "FamilyData/CR3BPEML2Lyapunovs.csv"
-p1::Int64, q1::Int64 = 1, 1
+p1::Int64, q1::Int64 = 3, 2
 guessOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q1/p1)
-orbit1::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit1, p1, q1, Deltaeps = 0.01, tol = 1E-11, refTol = 1E-11, JTol = 2E-3)
+orbit1::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit1, p1, q1, Deltaeps = 0.001, tol = 1E-9, refTol = 1E-9, JTol = 1E-2)
 compOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q1/p1)
-propTime::Float64 = pi*5
-BCR4BPManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit1, "Unstable", "Negative", 25/get12CharLength(systemData), 100*p1)
+propTime::Float64 = pi*3
+BCR4BPManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit1, "Unstable", "Positive", 25/get12CharLength(systemData), 100*p1)
 BCR4BPManifold.TOF = propTime
-CR3BPManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit1, "Unstable", "Negative", 25/getCharLength(CR3BPSystemData), 100)
+CR3BPManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit1, "Unstable", "Positive", 25/getCharLength(CR3BPSystemData), 100)
 CR3BPManifold.TOF = propTime
-pseudoManifold::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit1, dynamicsModel, pi*0/2, 100)
+pseudoManifold::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit1, dynamicsModel, pi*1/2, 100)
 pseudoManifold.TOF = pi*2
 
 mf = MATLAB.MatFile("Output/ManifoldComparison.mat", "w")
