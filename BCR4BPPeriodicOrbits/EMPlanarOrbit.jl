68aa76c7cd9bb71a820a0677439811f4feba03db
Jonathan Richmond <richmo11@purdue.edu>
2025-06-10 23:44:34 -0400

    Obtained L2 Lyapunovs

--- BCR4BPPeriodicOrbits/EMPlanarOrbit.jl
+++ BCR4BPPeriodicOrbits/EMPlanarOrbit.jl
@@ -26,17 +26,18 @@ propagator = MBD.Propagator()
 targeter = PlanarPerpP12Targeter(dynamicsModel)
 CR3BPTargeter = PlanarPerpJCTargeter(CR3BPDynamicsModel)
 
+familyFile::String = "FamilyData/CR3BPEML2Lyapunovs.csv"
 CR3BPP::Float64, targetP::Float64 = 6.2840049277323855, getSynodicPeriod(dynamicsModel)
 
 p1::Int64, q1::Int64 = 1, 1
-guessOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, "FamilyData/CR3BPEML1Lyapunovs.csv", "Period", CR3BPP*q1/p1)
+guessOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q1/p1)
 orbit1::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit1, p1, q1, Deltaeps = 0.01, tol = 1E-11, refTol = 1E-11, JTol = 2E-3)
-compOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, "FamilyData/CR3BPEML1Lyapunovs.csv", "Period", targetP*q1/p1)
+compOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q1/p1)
 
 p2::Int64, q2::Int64 = 3, 2
-guessOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, "FamilyData/CR3BPEML1Lyapunovs.csv", "Period", CR3BPP*q2/p2)
+guessOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q2/p2)
 orbit2::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit2, p2, q2, Deltaeps = 0.001, tol = 1E-9, refTol = 1E-9, JTol = 1E-2)
-compOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, "FamilyData/CR3BPEML1Lyapunovs.csv", "Period", targetP*q2/p2)
+compOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q2/p2)
 
 mf = MATLAB.MatFile("Output/EMPlanarOrbit.mat", "w")
 exportBCR4BP12Orbit(orbit1, mf, :BCR4BPOrbit1)
