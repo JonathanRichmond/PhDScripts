2614e50d14af6aacfe43f77e2889b5ce6e3ed77f
Jonathan Richmond <richmo11@purdue.edu>
2025-06-23 16:41:17 -0400

    Updated homotopy process

--- BCR4BPPeriodicOrbits/EMSpatialOrbit.jl
+++ BCR4BPPeriodicOrbits/EMSpatialOrbit.jl
@@ -3,7 +3,7 @@ Script for BCR4BP Earth-Moon spatial orbits
 
 Author: Jonathan Richmond
 C: 6/11/25
-U: 6/18/25
+U: 6/23/25
 """
 module EMSpatial
 println()
@@ -28,29 +28,17 @@ targeter = SpatialPerpP12Targeter(dynamicsModel)
 CR3BPTargeter = SpatialPerpVyTargeter(CR3BPDynamicsModel)
 
 familyFile::String = "FamilyData/CR3BPEML2Halos.csv"
-CR3BPP::Float64, targetP::Float64 = 6.2840049277323855, getSynodicPeriod(dynamicsModel)
-
-p1::Int64, q1::Int64 = 3, 1
-guessOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q1/p1; choiceIndex = 1)
-orbit1::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit1, 0.0, p1, q1, Deltaeps = 0.001, tol = 1E-10, refTol = 1E-10, JTol = 2E-3)
-compOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q1/p1; choiceIndex = 1)
-(orbit1ICSB1::Vector{Vector{Float64}}, ~) = rotating12ToRotating41(dynamicsModel, [orbit1.initialCondition], [0.0])
-
-p2::Int64, q2::Int64 = 3, 1
-guessOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q2/p2)
-orbit2::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit2, pi*1.0, p2, q2, Deltaeps = 0.001, tol = 1E-10, refTol = 1E-10, JTol = 2E-3)
-compOrbit2::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q2/p2)
-(orbit2ICSB1::Vector{Vector{Float64}}, ~) = rotating12ToRotating41(dynamicsModel, [orbit2.initialCondition], [0.0])
+p::Int64, q::Int64 = 3, 1
+compOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", getSynodicPeriod(dynamicsModel)*q/p; choiceIndex = 1)
+println("Converged $p:$q CR3BP Orbit:\n\tIC:\t$(compOrbit.initialCondition)\n\tP:\t$(compOrbit.period)\n")
+q0JumpCheck = MBD.BoundingBoxJumpCheck("IntialState", [0.8 1.2; 0.0 0.1; 0.5 1.0])
+orbit::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, compOrbit, 0.0, p, q, q0JumpCheck, tol = 1E-10, JTol = 2E-2)
+(orbitSB1::Vector{Vector{Float64}}, ~) = rotating12ToRotating41(dynamicsModel, [orbit.initialCondition], [0.0])
 
 mf = MATLAB.MatFile("Output/EMSpatialOrbit.mat", "w")
-exportBCR4BP12Orbit(orbit1, mf, :BCR4BPOrbit1)
-exportCR3BPOrbit(guessOrbit1, mf, :CR3BPGuessOrbit1)
-exportCR3BPOrbit(compOrbit1, mf, :CR3BPCompOrbit1)
-exportBCR4BP41Trajectory(orbit1ICSB1[1]..., orbit1.period*get12CharTime(systemData)/get41CharTime(systemData), SB1DynamicsModel, mf, :BCR4BPSB1Orbit1)
-exportBCR4BP12Orbit(orbit2, mf, :BCR4BPOrbit2)
-exportCR3BPOrbit(guessOrbit2, mf, :CR3BPGuessOrbit2)
-exportCR3BPOrbit(compOrbit2, mf, :CR3BPCompOrbit2)
-exportBCR4BP41Trajectory(orbit2ICSB1[1]..., orbit2.period*get12CharTime(systemData)/get41CharTime(systemData), SB1DynamicsModel, mf, :BCR4BPSB1Orbit2)
+exportCR3BPOrbit(compOrbit, mf, :CR3BPCompOrbit)
+exportBCR4BP12Orbit(orbit, mf, :BCR4BPOrbit)
+exportBCR4BP41Trajectory(orbitSB1[1]..., orbit.period*get12CharTime(systemData)/get41CharTime(systemData), SB1DynamicsModel, mf, :BCR4BPSB1Orbit)
 MATLAB.close(mf)
 
 println()
--- Scripts/ManifoldComparison.jl
+++ Scripts/ManifoldComparison.jl
@@ -3,7 +3,7 @@ Script for comparing CR3BP and BCR4BP invariant and pseudo-manifolds
 
 Author: Jonathan Richmond
 C: 6/10/25
-U: 6/18/25
+U: 6/23/25
 """
 module ManComp
 println()
@@ -26,45 +26,44 @@ propagator = MBD.Propagator()
 targeter = PlanarPerpP12Targeter(dynamicsModel)
 CR3BPTargeter = PlanarPerpJCTargeter(CR3BPDynamicsModel)
 
-CR3BPP::Float64, targetP::Float64 = 6.2840049277323855, getSynodicPeriod(dynamicsModel)
-
 familyFile::String = "FamilyData/CR3BPEML2Lyapunovs.csv"
-p1::Int64, q1::Int64 = 2, 1
-guessOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", CR3BPP*q1/p1)
-orbit1::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, guessOrbit1, 0.0, p1, q1, Deltaeps = 0.01, tol = 1E-11, refTol = 1E-11, JTol = 2E-3)
-compOrbit1::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", targetP*q1/p1)
+p::Int64, q::Int64 = 2, 1
+compOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(CR3BPTargeter, familyFile, "Period", getSynodicPeriod(dynamicsModel)*q/p; choiceIndex = 1)
+println("Converged $p:$q CR3BP Orbit:\n\tIC:\t$(compOrbit.initialCondition)\n\tP:\t$(compOrbit.period)\n")
+q0JumpCheck = MBD.BoundingBoxJumpCheck("IntialState", [0.9 1.3; 0.0 0.5])
+orbit::MBD.BCR4BP12PeriodicOrbit = getResonantOrbit(targeter, compOrbit, 0.0, p, q, q0JumpCheck)
 
-propTime::Float64 = pi*5
-BCR4BPPosManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit1, "Unstable", "Positive", 25/get12CharLength(systemData), 100*p1)
+propTime::Float64 = pi*2
+BCR4BPPosManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit, "Unstable", "Positive", 25/get12CharLength(systemData), 100*p)
 BCR4BPPosManifold.TOF = propTime
 BCR4BPPosManifoldArcs::Vector{MBD.BCR4BP12ManifoldArc} = stopCrashes(BCR4BPPosManifold)
-BCR4BPNegManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit1, "Unstable", "Negative", 25/get12CharLength(systemData), 100*p1)
+BCR4BPNegManifold::MBD.BCR4BP12Manifold = getManifoldByArclength(orbit, "Unstable", "Negative", 25/get12CharLength(systemData), 100*p)
 BCR4BPNegManifold.TOF = propTime
 BCR4BPNegManifoldArcs::Vector{MBD.BCR4BP12ManifoldArc} = stopCrashes(BCR4BPNegManifold)
 BCR4BPManifoldArcs::Vector{MBD.BCR4BP12ManifoldArc} = append!(BCR4BPPosManifoldArcs, BCR4BPNegManifoldArcs)
-CR3BPPosManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit1, "Unstable", "Positive", 25/getCharLength(CR3BPSystemData), 100)
+CR3BPPosManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit, "Unstable", "Positive", 25/getCharLength(CR3BPSystemData), 100)
 CR3BPPosManifold.TOF = propTime
 CR3BPPosManifoldArcs::Vector{MBD.CR3BPManifoldArc} = stopCrashes(CR3BPPosManifold)
-CR3BPNegManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit1, "Unstable", "Negative", 25/getCharLength(CR3BPSystemData), 100)
+CR3BPNegManifold::MBD.CR3BPManifold = getManifoldByArclength(compOrbit, "Unstable", "Negative", 25/getCharLength(CR3BPSystemData), 100)
 CR3BPNegManifold.TOF = propTime
 CR3BPNegManifoldArcs::Vector{MBD.CR3BPManifoldArc} = stopCrashes(CR3BPNegManifold)
 CR3BPManifoldArcs::Vector{MBD.CR3BPManifoldArc} = append!(CR3BPPosManifoldArcs, CR3BPNegManifoldArcs)
-pseudoManifold0::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit1, dynamicsModel, pi*0/2, 100)
-pseudoManifold0.TOF = pi*2
-pseudoManifoldArcs0::Vector{MBD.BCR4BPPseudoManifoldArc} = stopCrashes(pseudoManifold0)
+pseudoManifold0::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit, dynamicsModel, 0.0, 50)
+pseudoManifold0.TOF = pi*1.5
 theta40::Vector{Float64} = collect(range(0, 2*pi, 21))
 pseudoManifoldArcs::Vector{MBD.BCR4BPPseudoManifoldArc} = []
 for t::Float64 in theta40[1:end-1]
-    pseudoManifold::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit1, dynamicsModel, t, 50)
-    pseudoManifold.TOF = pi*2
+    pseudoManifold::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit, dynamicsModel, t, 50)
+    pseudoManifold.TOF = pseudoManifold0.TOF
     pseudoManifoldArcsSub::Vector{MBD.BCR4BPPseudoManifoldArc} = stopCrashes(pseudoManifold)
     append!(pseudoManifoldArcs, pseudoManifoldArcsSub)
 end
-pseudoManifold0::MBD.BCR4BPPseudoManifold = getPseudoManifoldByArclength(compOrbit1, dynamicsModel, 0.0, 50)
 
 mf = MATLAB.MatFile("Output/ManifoldComparison.mat", "w")
-exportBCR4BP12Manifold(BCR4BPPosManifold, BCR4BPManifoldArcs, mf, :BCR4BPManifold)
+exportCR3BPOrbit(compOrbit, mf, :CR3BPCompOrbit)
+exportBCR4BP12Orbit(orbit, mf, :BCR4BPOrbit)
 exportCR3BPManifold(CR3BPPosManifold, CR3BPManifoldArcs, mf, :CR3BPManifold)
+exportBCR4BP12Manifold(BCR4BPPosManifold, BCR4BPManifoldArcs, mf, :BCR4BPManifold)
 exportPseudoManifold(pseudoManifold0, pseudoManifoldArcs, mf, :pseudoManifold)
 MATLAB.close(mf)
 
