f55c416383ef4cc9aeddd28cd14c5987a99b5c25
Jonathan Richmond <richmo11@purdue.edu>
2025-07-16 17:48:41 -0400

    Updated Visibility script

--- RA/Katalyst/Visibility.jl
+++ RA/Katalyst/Visibility.jl
@@ -1,50 +1,39 @@
 """
-Script for computing orbit metrics
+Script for computing visibility
 
 Author: Jonathan Richmond
 C: 7/7/25
-U: 7/10/25
+U: 7/16/25
 """
 module Vis
 println()
 
-using MBD, LinearAlgebra, Logging, MATLAB
+using MBD, LinearAlgebra, Logging, MATLAB, Statistics
 using Base.Threads
 
 global_logger(ConsoleLogger(stderr, Logging.Warn)) # Debug, Info, Warn, Error
 
 include("../../CR3BPTargeters/PlanarLFRX.jl")
+include("../../CR3BPTargeters/PlanarPerpJC.jl")
+include("../../CR3BPTargeters/SpatialPerpJCMS.jl")
 include("../../CR3BPTargeters/SpatialPerpVy.jl")
 include("../../Utilities/Export.jl")
+include("../../Utilities/Visibility.jl")
 
-function checkExclusion(r_O::Vector{Float64}, r_T::Vector{Float64}, primaryPos::Vector{Float64}, sigma::Float64)
-    r_OT::Vector{Float64} = r_T-r_O
-    r_OP::Vector{Float64} = primaryPos-r_O
-    kappa::Float64 = acos(LinearAlgebra.dot(r_OT, r_OP)/(LinearAlgebra.norm(r_OT)*LinearAlgebra.norm(r_OP)))
+function sampleRegion(nPoints::Int64, axisLimits::Matrix{Float64}, r_E::Vector{Float64}, r_M::Vector{Float64}, R_E::Float64, R_M::Float64)
+    points::Matrix{Float64} = zeros(Float64, (3,nPoints))
+    pointCount::Int64 = 0
+    while pointCount < nPoints
+        point::Vector{Float64} = axisLimits[:,1] + rand(3) .* (axisLimits[:,2]-axisLimits[:,1])
+        # radius::Float64 = 2*(R_E+(R_M-R_E)*(point[1]-axisLimits[1,1]))
+        radius::Float64 = (point[1]-axisLimits[1,1])*tan(10*pi/180)
+        if (LinearAlgebra.norm(point[2:3]) <= radius) && (LinearAlgebra.norm(point-r_E) > R_E) && (LinearAlgebra.norm(point-r_M) > R_M)
+            @inbounds points[:,pointCount] = point
+            pointCount += 1
+        end
+    end
 
-    return (abs(kappa) <= sigma)
-end
-
-function getLunarExclusionAngle(dynamicsModel::MBD.CR3BPDynamicsModel, r_O::Vector{Float64}, MoonPos::Vector{Float64}, SunPos::Vector{Float64}, sigmaMDeg::Float64)
-    r_OM::Vector{Float64} = MoonPos-r_O
-    r_OS::Vector{Float64} = SunPos-r_O
-    Chi::Float64 = acos(LinearAlgebra.dot(r_OM, r_OS)/(LinearAlgebra.norm(r_OM)*LinearAlgebra.norm(r_OS)))
-
-    return sigmaMDeg*Chi/180+atan(dynamicsModel.systemData.primaryData[2].bodyRadius/(getCharLength(dynamicsModel)*LinearAlgebra.norm(MoonPos-r_O)))
-end
-
-function visibility(dynamicsModel::MBD.CR3BPDynamicsModel, R4BPModel::MBD.BCR4BP12DynamicsModel, r_O::Vector{Float64}, r_T::Vector{Float64}, objR::Float64, Cd::Float64, thetaS::Float64)
-    SunPos::Vector{Float64} = getPrimaryState(R4BPModel, 4, thetaS)[1:3]
-    isEarthExcluded = checkExclusion(r_O, r_T, getPrimaryState(dynamicsModel, 1)[1:3], 10*pi/180)
-    isMoonExcluded = checkExclusion(r_O, r_T, getPrimaryState(dynamicsModel, 2)[1:3], getLunarExclusionAngle(dynamicsModel, r_O, getPrimaryState(dynamicsModel, 2)[1:3], SunPos, 10.0))
-    isSunExcluded = checkExclusion(r_O, r_T, SunPos, 30*pi/180)
-    (isEarthExcluded || isMoonExcluded || isSunExcluded) && return 100.0
-
-    r_TO::Vector{Float64} = r_O-r_T
-    r_TS::Vector{Float64} = SunPos-r_T
-    alpha::Float64 = acos(LinearAlgebra.dot(r_TO, r_TS)/(LinearAlgebra.norm(r_TO)*LinearAlgebra.norm(r_TS)))
-
-    return -26.832-2.5*log10(LinearAlgebra.norm(r_T-SunPos)^2*2*objR^2*Cd*(sin(alpha)+(pi-alpha)*cos(alpha))/(get4Distance(R4BPModel)^2*2*pi*LinearAlgebra.norm(-r_TO)^2*3*pi))
+    return points
 end
 
 systemData = MBD.CR3BPSystemData("Earth", "Moon")
@@ -53,70 +42,75 @@ BCR4BPSystemData = MBD.BCR4BPSystemData("Earth", "Moon", "Sun", "Earth_Barycente
 BCR4BPDynamicsModel = MBD.BCR4BP12DynamicsModel(BCR4BPSystemData)
 L2::Vector{Float64} = getEquilibriumPoint(dynamicsModel, 2)
 
-obsTargeter = SpatialPerpVyTargeter(dynamicsModel)
-tarTargeter = PlanarLFRXTargeter(dynamicsModel)
+# obsTargeter = SpatialPerpVyTargeter(dynamicsModel)
+# obsTargeter = PlanarPerpJCTargeter(dynamicsModel)
+obsTargeter = SpatialPerpJCMSTargeter(dynamicsModel)
+targTargeter = PlanarLFRXTargeter(dynamicsModel)
 propagator = MBD.Propagator()
 tSyn::Float64 = getSynodicPeriod(BCR4BPDynamicsModel)
 
-p::Int64 = 3
-q::Int64 = 5
-obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEMResonant3_2ProsSpatial.csv", "Period", tSyn*q/p)
+p::Int64 = 2
+q::Int64 = 7
+# obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEMResonant3_2ProsSpatial.csv", "Period", tSyn*q/p)
+# obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEML1Halos.csv", "Period", tSyn*q/p)
+# obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEML2Halos.csv", "Period", tSyn*q/p)
+# obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEMResonant2_1Rets.csv", "Period", tSyn*q/p)
+# obsOrbit::MBD.CR3BPPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEMResonant3_2Pros.csv", "Period", tSyn*q/p)
+# obsOrbit::MBD.CR3BPMSPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEMCyclersSpatial.csv", "Period", tSyn*q/p, 3)
+obsOrbit::MBD.CR3BPMSPeriodicOrbit = interpOrbit(obsTargeter, "FamilyData/CR3BPEML1P7HO1s.csv", "Period", tSyn*q/p, 15)
 println("Converged Observer Orbit:\n\tIC:\t$(obsOrbit.initialCondition)\n\tP:\t$(obsOrbit.period) ($(obsOrbit.period*getCharTime(systemData)/3600/24) d.)\n\tJC:\t$(getJacobiConstant(obsOrbit))\n\tStab.:\t$(getStabilityIndex(obsOrbit))\n")
-tarTraj::MBD.CR3BPMultipleShooterProblem = interpSolution(tarTargeter, "FamilyData/CR3BPEMLFRPros.csv", "x", getPrimaryState(dynamicsModel, 2)[1]+100/getCharLength(systemData), 3, 185/getCharLength(systemData), 0.0)
-println("Converged Target Trajectory:\n\tIC:\t$(tarTraj.nodes[end].state.data[1:6])\n\tTOF:\t$(getTOF(tarTargeter, tarTraj)) ($(getTOF(tarTargeter, tarTraj)*getCharTime(systemData)/3600/24) d.)\n\tJC:\t$(getJacobiConstant(dynamicsModel, tarTraj.nodes[1].state.data[1:6]))\n")
+targTraj::MBD.CR3BPMultipleShooterProblem = interpSolution(targTargeter, "FamilyData/CR3BPEMLFRPros.csv", "x", getPrimaryState(dynamicsModel, 2)[1]+100/getCharLength(systemData), 3, 185/getCharLength(systemData), 0.0)
+println("Converged Target Trajectory:\n\tIC:\t$(targTraj.nodes[end].state.data[1:6])\n\tTOF:\t$(getTOF(targTargeter, targTraj)) ($(getTOF(targTargeter, targTraj)*getCharTime(systemData)/3600/24) d.)\n\tJC:\t$(getJacobiConstant(dynamicsModel, targTraj.nodes[1].state.data[1:6]))\n")
 
-r_E::Vector{Float64} = getPrimaryState(dynamicsModel, 1)[1:3]
-r_M::Vector{Float64} = getPrimaryState(dynamicsModel, 2)[1:3]
-R_E::Float64 = systemData.primaryData[1].bodyRadius/getCharLength(systemData)
-R_M::Float64 = systemData.primaryData[2].bodyRadius/getCharLength(systemData)
-points::Vector{Vector{Float64}} = []
-while length(points) < 6000
-    d::Float64 = rand()*(L2[1]-r_E[1])
-    theta::Float64 = rand()*2.0*pi
-    # radius::Float64 = sqrt(rand())*2*(R_E+(R_M-R_E)*d)
-    radius::Float64 = sqrt(rand())*d*tan(10*pi/180)
-    point::Vector{Float64} = [r_E[1]+d, radius*cos(theta), radius*sin(theta)]
-    ((LinearAlgebra.norm(point-r_E) > R_E) && (LinearAlgebra.norm(point-r_M) > R_M)) && push!(points, point)
-end
+println("Sampling region...")
+nPoints::Int64 = 20000
+EarthPos::Vector{Float64} = getPrimaryState(dynamicsModel, 1)[1:3]
+MoonPos::Vector{Float64} = getPrimaryState(dynamicsModel, 2)[1:3]
+r_E::Float64 = systemData.primaryData[1].bodyRadius/getCharLength(systemData)
+r_M::Float64 = systemData.primaryData[2].bodyRadius/getCharLength(systemData)
+# maxValue::Float64 = 2*r_E
+maxValue::Float64 = (L2[1]-EarthPos[1])*tan(10*pi/180)
+region::Matrix{Float64} = sampleRegion(nPoints, [EarthPos[1] L2[1]; -maxValue maxValue; -maxValue maxValue], EarthPos, MoonPos, r_E, r_M)
 
-tarR::Float64 = 3.54E-3/getCharLength(systemData)
+targR::Float64 = 3.5E-3/getCharLength(systemData)
 Cd::Float64 = 0.5
-thetaS0::Float64 = 0.0
-xi0::Float64 = 0*2*pi/q/4
+thetaS0::Vector{Float64} = collect(range(0, 2*pi, 91))
+xi0::Float64 = 0.0
+times::Vector{Float64} = collect(range(0, tSyn, 501))
 obsICs::Vector{Vector{Float64}} = Vector{Vector{Float64}}(undef, q)
+obsArcs::Vector{MBD.CR3BPArc} = Vector{MBD.CR3BPArc}(undef, q)
 for o::Int64 = 1:q
     xi::Float64 = xi0+2*pi*(o-1)/q
     orbitArc::MBD.CR3BPArc = propagate(propagator, obsOrbit.initialCondition, [0, xi*obsOrbit.period/(2*pi)], dynamicsModel)
-    obsICs[o] = getStateByIndex(orbitArc, -1)
+    obsICs[o] = getStateByIndex(orbitArc, -1)[1:6]
+    obsArcs[o] = propagate(propagator, obsICs[o], times, dynamicsModel)
 end
-times::Vector{Float64} = collect(range(0, tSyn, 501))
-V::Vector{Float64} = Vector{Float64}(undef, 501)
-for t::Int64 = 1:length(times)
-    thetaS::Float64 = thetaS0+evaluateEquations(BCR4BPDynamicsModel, MBD.SIMPLE, times[t], [0.8, 0, 0, 0, 0, 0, thetaS0])[7]*times[t]
-    totalVis::Vector{Bool} = Vector{Bool}(undef, length(points))
-    Threads.@threads for p::Int64 = 1:length(points)
-        r_T::Vector{Float64} = points[p]
-        vis::Bool = false
-        for o::Int64 = 1:q
-            arc::MBD.CR3BPArc = propagate(propagator, obsICs[o], [0, times[t]], dynamicsModel)
-            r_O::Vector{Float64} = getStateByIndex(arc, -1)[1:3]
-            if visibility(dynamicsModel, BCR4BPDynamicsModel, r_O, r_T, tarR, Cd, thetaS) <= 18.0
-                vis = true
-                break
-            end
-        end
-        totalVis[p] = vis
-    end
-    V[t] = 100.0*count(identity, totalVis)/length(totalVis)
+
+println("Analyzing phasing and visibility...")
+thetaSdot::Float64 = evaluateEquations(BCR4BPDynamicsModel, MBD.SIMPLE, 0.0, [0.8, 0, 0, 0, 0, 0, thetaS0[1]])[7]
+V::Matrix{Float64} = zeros(Float64, (length(thetaS0),length(times)))
+Threads.@threads for index::Int64 = 1:(length(thetaS0)*length(times))
+    theta::Int64 = div(index-1, length(times))+1
+    t::Int64 = mod(index-1, length(times))+1
+    thetaS::Float64 = thetaS0[theta]+thetaSdot*(times[t]-times[1])
+    r_O::Matrix{Float64} = hcat([getStateByIndex(obsArcs[o], findfirst(obsArcs[o].times .== times[t]))[1:3] for o = 1:q]...)
+    brightness::Matrix{Float64} = visibility(r_O, region, EarthPos, MoonPos, getPrimaryState(BCR4BPDynamicsModel, 4, thetaS)[1:3], 10*pi/180, 10*pi/180, 30*pi/180, r_M, getCharLength(systemData), targR, Cd, get4Distance(BCR4BPDynamicsModel))
+    vis::Vector{Bool} = vec(any(brightness .<= 18.0, dims = 1))
+    V[theta,t] = 100.0*count(vis)/nPoints
 end
+avgV::Vector{Float64} = vec(Statistics.mean(V, dims = 2))
+maxIndex::Int64 = argmax(avgV)
 
 mf = MATLAB.MatFile("RA/Katalyst/Visibility.mat", "w")
 exportCR3BPOrbit(obsOrbit, mf, :ObserverOrbit)
 exportArrays(obsICs, mf, :ObserverICs)
 # exportCR3BPTrajectory(tarTraj, mf, :TargetTrajectory)
-exportArrays(points, mf, :Region)
+MATLAB.put_variable(mf, :Region, region)
+MATLAB.put_variable(mf, :SunAngle, thetaS0)
+MATLAB.put_variable(mf, :AverageVisibility, avgV)
+MATLAB.put_variable(mf, :Index, maxIndex)
 MATLAB.put_variable(mf, :Times, times./tSyn)
-MATLAB.put_variable(mf, :Visible, V)
+MATLAB.put_variable(mf, :Visible, V[maxIndex,:])
 MATLAB.close(mf)
 
 println()
