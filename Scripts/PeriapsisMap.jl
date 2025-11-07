55a24a06bf8a90deed57d8824a8d73a685325646
Jonathan Richmond <richmo11@purdue.edu>
2025-11-07 14:58:36 -0500

    Updated PeriapsisMap script

--- Scripts/PeriapsisMap.jl
+++ Scripts/PeriapsisMap.jl
@@ -3,7 +3,7 @@ Script for computing periapsis maps
 
 Author: Jonathan Richmond
 C: 10/9/25
-U: 11/4/25
+U: 11/7/25
 """
 # module PeriMap
 println("Running PeriapsisMap.jl...\n")
@@ -21,7 +21,7 @@ mutable struct Periapsis
 end
 
 function endConditionsBCR4BP(out::SubArray{Float64}, state::Vector{Float64}, time::Float64, integrator)
-    if time < 1E-8
+    if time < 1E-10
         out[1:4] = [1.0, 1.0, 1.0, 1.0]
     else
         q_B1::Vector{Float64} = [1-integrator.p[4], 0, 0, 0, 0, 0]
@@ -37,7 +37,7 @@ function endConditionsBCR4BP(out::SubArray{Float64}, state::Vector{Float64}, tim
 end
 
 function endConditionsCR3BP(out::SubArray{Float64}, state::Vector{Float64}, time::Float64, integrator)
-    if time < 1E-8
+    if time < 1E-10
         out[1:4] = [1.0, 1.0, 1.0, 1.0]
     else
         q_E::Vector{Float64} = getPrimaryState(integrator.p[3], 1)
@@ -51,7 +51,7 @@ function endConditionsCR3BP(out::SubArray{Float64}, state::Vector{Float64}, time
 end
 
 function periapsisCondition(state::Vector{Float64}, time::Float64, integrator)
-    if abs(time) < 1E-8
+    if abs(time) < 1E-10
         -1.0
     else
         LinearAlgebra.dot(state[1:3], state[4:6])
@@ -61,19 +61,9 @@ end
 function endAffectBCR4BP!(integrator, index)
     if index == 1
         integrator.p[8].count += 1
-        if integrator.p[8].count >= 3
-            integrator.p[2].flag = :capture
-            DifferentialEquations.terminate!(integrator)
-        end
     else
         if index == 2
-            if integrator.p[8].count == 0
-                integrator.p[2].flag = :escape
-            elseif integrator.p[8].count == 1
-                integrator.p[2].flag = :escape1
-            elseif integrator.p[8].count == 2
-                integrator.p[2].flag = :escape2
-            end
+            integrator.p[2].flag = Symbol("escape", integrator.p[8].count)
         elseif index == 3
             integrator.p[2].flag = :earth
         elseif index == 4
@@ -86,19 +76,9 @@ end
 function endAffectCR3BP!(integrator, index)
     if index == 1
         integrator.p[7].count += 1
-        if integrator.p[7].count >= 3
-            integrator.p[2].flag = :capture
-            DifferentialEquations.terminate!(integrator)
-        end
     else
         if index == 2
-            if integrator.p[7].count == 0
-                integrator.p[2].flag = :escape
-            elseif integrator.p[7].count == 1
-                integrator.p[2].flag = :escape1
-            elseif integrator.p[7].count == 2
-                integrator.p[2].flag = :escape2
-            end
+            integrator.p[2].flag = Symbol("escape", integrator.p[7].count)
         elseif index == 3
             integrator.p[2].flag = :earth
         elseif index == 4
@@ -114,6 +94,55 @@ function endAffectManifold!(integrator)
     if integrator.p[2].count >= 6
         DifferentialEquations.terminate!(integrator)
     end
+end
+
+function matRotating12ToRotating41(dynamicsModel::MBD.BCR4BP12DynamicsModel, states12::Vector{Vector{Float64}}, times12::Vector{Float64})
+    numStates::Int64 = length(states12)
+    states12_mat::Matrix{Float64} = Matrix{Float64}(undef, 7, numStates)
+    Threads.@threads for s::Int64 in 1:numStates
+        @inbounds @simd for j in 1:7
+            states12_mat[j,s] = states12[s][j]
+        end
+    end
+    theta4::Float64 = states12[1][7]
+    mu41::Float64 = get41MassRatio(dynamicsModel.systemData)
+    m4::Float64 = get4Mass(dynamicsModel.systemData)
+    a4::Float64 = get4Distance(dynamicsModel.systemData)
+    theta4dot::Float64 = sqrt((m4+1)/(a4^3))-1
+    C::Matrix{Float64} = [-cos(theta4) -sin(theta4) 0; sin(theta4) -cos(theta4) 0; 0 0 1]
+    Cdot::Matrix{Float64} = theta4dot.*[sin(theta4) -cos(theta4) 0; cos(theta4) sin(theta4) 0; 0 0 0]
+    N::Matrix{Float64} = [(1/a4).*C zeros(Float64, 3, 4); sqrt(a4/(m4+1)).*Cdot sqrt(a4/(m4+1)).*C zeros(Float64, 3, 1); zeros(Float64, 1, 6) -1]
+    rho::Vector{Float64} = [1-mu41; zeros(Float64, 5); pi]
+    states41_mat::Matrix{Float64} = N*states12_mat .+ rho
+    states41::Vector{Vector{Float64}} = [@view states41_mat[:, c] for c in axes(states41_mat, 2)]
+    times41::Vector{Float64} = (get12CharTime(dynamicsModel.systemData)/get41CharTime(dynamicsModel.systemData)).*times12
+
+    return (states41, times41)
+end
+
+function matRotating41ToRotating12(dynamicsModel::MBD.BCR4BP41DynamicsModel, states41::Vector{Vector{Float64}}, times41::Vector{Float64})
+    numStates::Int64 = length(states41)
+    states41_mat::Matrix{Float64} = Matrix{Float64}(undef, 7, numStates)
+    Threads.@threads for s::Int64 in 1:numStates
+        @inbounds @simd for j in 1:7
+            states41_mat[j,s] = states41[s][j]
+        end
+    end
+    theta2::Float64 = states41[1][7]
+    mu41::Float64 = get41MassRatio(dynamicsModel.systemData)
+    m4::Float64 = get4Mass(dynamicsModel.systemData)
+    a4::Float64 = get4Distance(dynamicsModel.systemData)
+    theta2dot::Float64 = sqrt((a4^3)/(m4+1))-1
+    C::Matrix{Float64} = [cos(theta2) sin(theta2) 0; -sin(theta2) cos(theta2) 0; 0 0 1]
+    Cdot::Matrix{Float64} = theta2dot.*[-sin(theta2) cos(theta2) 0; -cos(theta2) -sin(theta2) 0; 0 0 0]
+    N::Matrix{Float64} = [a4.*C zeros(Float64, 3, 4); sqrt((m4+1)/a4).*Cdot sqrt((m4+1)/a4).*C zeros(Float64, 3, 1); zeros(Float64, 1, 6) -1]
+    rho::Vector{Float64} = [-1+mu41; zeros(Float64, 6)]
+    psi::Vector{Float64} = [zeros(Float64, 6); pi]
+    states12_mat::Matrix{Float64} = N*(states41_mat .+ rho) .+ psi
+    states12::Vector{Vector{Float64}} = [@view states12_mat[:, c] for c in axes(states12_mat, 2)]
+    times12::Vector{Float64} = (get41CharTime(dynamicsModel.systemData)/get12CharTime(dynamicsModel.systemData)).*times41
+
+    return (states12, times12)
 end
 
 mf = MATLAB.MatFile("Output/PeriapsisMap.mat", "w")
@@ -151,7 +180,7 @@ rhatSB1::Vector{StaticArrays.SVector{2, Float64}} = deltarSB1 ./ rSB1
 thatSB1::Vector{StaticArrays.SVector{2, Float64}} = [StaticArrays.SVector(-v[2], v[1]) for v in rhatSB1] # Prograde
 # thatSB1::Vector{StaticArrays.SVector{2, Float64}} = [StaticArrays.SVector(v[2], -v[1]) for v in rhatSB1] # Retrograde
 qGuessSB1::Vector{Vector{Float64}} = [[posSB1[j]..., 0, thatSB1[j]..., 0, thetaM] for j in eachindex(posSB1)]
-qGuessEM::Vector{Vector{Float64}} = rotating41ToRotating12(SB1DynamicsModel, qGuessSB1, zeros(length(qGuessSB1)))[1]
+qGuessEM::Vector{Vector{Float64}} = matRotating41ToRotating12(SB1DynamicsModel, qGuessSB1, zeros(length(qGuessSB1)))[1]
 OmegaEM::Vector{Float64} = map(q -> getPseudopotential(EMCR3BPDynamicsModel, q[1:3]), qGuessEM)
 v2EM::Vector{Float64} = 2 .* OmegaEM .- JCEM
 v2EM[v2EM .< 0] .= NaN
@@ -159,27 +188,22 @@ vEM::Vector{Float64} = sqrt.(v2EM)
 Threads.@threads for j in eachindex(vEM)
     qGuessEM[j][4:5] = qGuessEM[j][4:5] .* vEM[j] ./ norm(qGuessEM[j][4:5])
 end
-qSB1::Vector{Vector{Float64}} = rotating12ToRotating41(EMDynamicsModel, qGuessEM, zeros(length(qGuessEM)))[1]
+qSB1::Vector{Vector{Float64}} = matRotating12ToRotating41(EMDynamicsModel, qGuessEM, zeros(length(qGuessEM)))[1]
 flagsSB1::Vector{Int64} = Vector{Int64}(undef, length(qSB1))
 Threads.@threads for q in eachindex(qSB1)
     if any(x -> isnan(x), qSB1[q])
-        flagsSB1[q] = 1
+        flagsSB1[q] = 7
     else
-        (arc::MBD.BCR4BP41Arc, event) = propagateWithEvents(propagator, endEventsBCR4BP, qSB1[q], [0, 4.0*pi], SB1DynamicsModel, [SB1DynamicsModel, get41MassRatio(systemData), R_H/get41CharLength(systemData), systemData.primaryData[1].bodyRadius/get41CharLength(systemData), systemData.primaryData[2].bodyRadius/get41CharLength(systemData), Periapsis(0, [])])
+        (arc::MBD.BCR4BP41Arc, event) = propagateWithEvents(propagator, endEventsBCR4BP, qSB1[q], [0, pi/2], SB1DynamicsModel, [SB1DynamicsModel, get41MassRatio(systemData), R_H/get41CharLength(systemData), systemData.primaryData[1].bodyRadius/get41CharLength(systemData), systemData.primaryData[2].bodyRadius/get41CharLength(systemData), Periapsis(0, [])])
         if event == :earth
-            flagsSB1[q] = 2
+            flagsSB1[q] = 8
         elseif event == :moon
-            flagsSB1[q] = 2
-        elseif event == :capture
-            flagsSB1[q] = 3
-        elseif event == :escape
-            flagsSB1[q] = 4
-        elseif event == :escape1
-            flagsSB1[q] = 5
-        elseif event == :escape2
-            flagsSB1[q] = 6
+            flagsSB1[q] = 8
+        elseif contains(String(event), "escape")
+            numPeris::RegexMatch{String} = match(r"\d+$", String(event))
+            flagsSB1[q] = parse(Int, numPeris.match)
         else
-            flagsSB1[q] = 0
+            flagsSB1[q] = 9
         end
     end
 end
@@ -203,52 +227,48 @@ end
 flags::Vector{Int64} = Vector{Int64}(undef, length(qEM))
 Threads.@threads for q in eachindex(qEM)
     if any(x -> isnan(x), qEM[q])
-        flags[q] = 1
+        flags[q] = 7
     else
-        (arc::MBD.CR3BPArc, event) = propagateWithEvents(propagator, endEventsCR3BP, qEM[q], [0, 4*pi*get41CharTime(systemData)/get12CharTime(systemData)], EMCR3BPDynamicsModel, [EMCR3BPDynamicsModel, R_H/get12CharLength(systemData), systemData.primaryData[1].bodyRadius/get12CharLength(systemData), systemData.primaryData[2].bodyRadius/get12CharLength(systemData), Periapsis(0, [])])
+        (arc::MBD.CR3BPArc, event) = propagateWithEvents(propagator, endEventsCR3BP, qEM[q], [0, pi/2*get41CharTime(systemData)/get12CharTime(systemData)], EMCR3BPDynamicsModel, [EMCR3BPDynamicsModel, R_H/get12CharLength(systemData), systemData.primaryData[1].bodyRadius/get12CharLength(systemData), systemData.primaryData[2].bodyRadius/get12CharLength(systemData), Periapsis(0, [])])
         if event == :earth
-            flags[q] = 2
+            flags[q] = 8
         elseif event == :moon
-            flags[q] = 2
-        elseif event == :capture
-            flags[q] = 3
-        elseif event == :escape
-            flags[q] = 4
-        elseif event == :escape1
-            flags[q] = 5
-        elseif event == :escape2
-            flags[q] = 6
+            flags[q] = 8
+        elseif contains(String(event), "escape")
+            numPeris::RegexMatch{String} = match(r"\d+$", String(event))
+            flags[q] = parse(Int, numPeris.match)
         else
-            flags[q] = 0
+            flags[q] = 9
         end
     end
 end
 
-targeter = PlanarPerpJCTargeter(EMCR3BPDynamicsModel)
-familyFile::String = "FamilyData/CR3BPEML2Lyapunovs.csv"
-orbit::MBD.CR3BPPeriodicOrbit = interpOrbit(targeter, familyFile, "JC", JCEM; choiceIndex = 1)
-manifold::MBD.CR3BPManifold = getManifoldByArclength(orbit, "Stable", "Negative", 25/get12CharLength(systemData), n*10)
-nThreads::Int64 = Threads.nthreads()
-qMan_thread::Vector{Vector{Vector{Float64}}} = [Vector{Vector{Float64}}() for _ in 1:nThreads]
-Threads.@threads for a in eachindex(manifold.orbitTimes)
-    tid = mod1(Threads.threadid(), nThreads)
-    peris = Periapsis(0, [])
-    arc::MBD.CR3BPArc = propagateWithEvent(propagator, manifoldEvent, real(manifold.initialConditions[a]), [0, -4*pi*get41CharTime(systemData)/get12CharTime(systemData)], EMCR3BPDynamicsModel, [peris])
-    for p in eachindex(peris.states)
-        push!(qMan_thread[tid], peris.states[p])
-    end
-end
-qMan::Vector{Vector{Float64}} = reduce(vcat, qMan_thread)
+# targeter = PlanarPerpJCTargeter(EMCR3BPDynamicsModel)
+# familyFile::String = "FamilyData/CR3BPEML2Lyapunovs.csv"
+# orbit::MBD.CR3BPPeriodicOrbit = interpOrbit(targeter, familyFile, "JC", JCEM; choiceIndex = 1)
+# manifold::MBD.CR3BPManifold = getManifoldByArclength(orbit, "Stable", "Negative", 25/get12CharLength(systemData), n*10)
+# nThreads::Int64 = Threads.nthreads()
+# qMan_thread::Vector{Vector{Vector{Float64}}} = [Vector{Vector{Float64}}() for _ in 1:nThreads]
+# Threads.@threads for a in eachindex(manifold.orbitTimes)
+#     tid = mod1(Threads.threadid(), nThreads)
+#     peris = Periapsis(0, [])
+#     arc::MBD.CR3BPArc = propagateWithEvent(propagator, manifoldEvent, real(manifold.initialConditions[a]), [0, -pi/2*get41CharTime(systemData)/get12CharTime(systemData)], EMCR3BPDynamicsModel, [peris])
+#     for p in eachindex(peris.states)
+#         push!(qMan_thread[tid], peris.states[p])
+#     end
+# end
+# qMan::Vector{Vector{Float64}} = reduce(vcat, qMan_thread)
 
-# sample::Int64 = 21567
-# (arc41::MBD.BCR4BP41Arc, event) = propagateWithEvents(propagator, endEventsBCR4BP, qSB1[sample], [0, 4.0*pi], SB1DynamicsModel, [SB1DynamicsModel, get41MassRatio(systemData), R_H/get41CharLength(systemData), systemData.primaryData[1].bodyRadius/get41CharLength(systemData), systemData.primaryData[2].bodyRadius/get41CharLength(systemData), Periapsis(0, [])])
+# sample::Int64 = 22296
+# (arc41::MBD.BCR4BP41Arc, event) = propagateWithEvents(propagator, endEventsBCR4BP, qSB1[sample], [0, pi/2], SB1DynamicsModel, [SB1DynamicsModel, get41MassRatio(systemData), R_H/get41CharLength(systemData), systemData.primaryData[1].bodyRadius/get41CharLength(systemData), systemData.primaryData[2].bodyRadius/get41CharLength(systemData), Periapsis(0, [])])
 # exportBCR4BP41Trajectory(qSB1[sample]..., getTimeByIndex(arc41, -1), SB1DynamicsModel, mf, :Traj41)
+# println(getTimeByIndex(arc41, -1))
 # q::Vector{Float64} = rotating41ToRotating12(SB1DynamicsModel, [qSB1[sample]], [0.0])[1][1]
 # exportBCR4BP12Trajectory(q..., getTimeByIndex(arc41, -1)*get41CharTime(systemData)/get12CharTime(systemData), EMDynamicsModel, mf, :Traj12)
 
 MATLAB.put_variable(mf, :pointsSB1, qSB1)
 MATLAB.put_variable(mf, :pointsEM, qEM)
-MATLAB.put_variable(mf, :manifold, qMan)
+# MATLAB.put_variable(mf, :manifold, qMan)
 MATLAB.put_variable(mf, :flagsSB1, flagsSB1)
 MATLAB.put_variable(mf, :flagsEM, flags)
 MATLAB.put_variable(mf, :JC, JCEM)
